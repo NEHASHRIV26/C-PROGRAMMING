22348bbcad780fb7c1e71cc4473fb8d1bd760160
NEHA SHRI V <nehashriv784@gmail.com>
2025-02-06 12:00:11 +0530

    Update c1.c

--- c1.c
+++ c1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int mark;
+    printf("enter mark:");
+    scanf("%d/n",&mark);
+if(mark>=90){
+    printf("grade A");
+}
+else if(mark<90 && mark >=80){
+    printf("grade B");
+}
+else if(mark<80  && mark >=70){
+    printf("grade C");
+}
+else if(mark<70 && mark>=60){
+    printf("grade D");
+}
+else if(mark<60 && mark>=50){
+    printf("grade E");
+}
+else{
+    printf("grade F");
+}
+return 0;
+}
+
 
