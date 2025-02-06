519f628088419e51b25eec3ff06f9d905f8709f3
NEHASHRIV26 <nehashriv784@gmail.com>
2025-02-06 11:52:27 +0530

    new files

---
+++ c10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i,j,arr[10];
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            scanf("%d\n",&i,&j);
+            printf("%d\n",i,j);
+        }
+    printf("%d\n",i,j);
+    }
+    return 0;
+}
---
+++ c8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int n,x,sum,number;
+    sum=0;
+    printf("enter number:");
+    scanf("%d\n",&number);
+    while(n>=0){
+        printf("")
+        x=sum+1;
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
